2c827086e16ad883f78c5ac9a1c8d274ed1c841e
Repus <congle310610@gmail.com>
2024-06-22 11:47:52 +1000

    This improved /shorten version run with 97 steps

--- SAT_3.py
+++ SAT_3.py
@@ -1,14 +1,21 @@
 from collections import defaultdict, Counter
 
 # Setting Up the Configuration
-###########0    1    2    3    4    5  
-board = [[".", ".", ".", ".", ".", "."], #0
-         [".", ".", ".", ".", ".", "."], #1
-         [".", ".", "1", ".", "1", "."], #2
-         [".", ".", ".", ".", ".", "."], #3
-         [".", ".", "1", ".", ".", "."], #4
-         [".", ".", ".", ".", ".", "."], #5
-]
+###########0    1    2    3    4    5    6    7    8    9    10    11
+board = [[".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."], #0
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "1", ".", "."], #0
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "1"], #2
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."], #3
+         [".", ".", ".", ".", ".", ".", ".", "1", ".", ".", ".", "."], #4
+         [".", ".", ".", "1", ".", ".", ".", ".", ".", ".", ".", "."], #5
+         [".", ".", ".", ".", ".", "1", ".", ".", ".", ".", ".", "."], #6
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "1", "."], #7
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."], #8
+         ["1", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."], #9
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."], #10
+         [".", ".", "1", ".", ".", ".", ".", ".", ".", ".", ".", "."], #11
+        ]
+
 # Finding Square that contain number
 setup_config = defaultdict(int)
 # ADT to store movement that are no longer possible
@@ -19,6 +26,7 @@ traverse = []
 count = 0
 surrounding = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 solved = False
+moved = []
 
 def layout_search():
     "Initital Search for number_square location"
@@ -30,41 +38,30 @@ def layout_search():
 def _okay_row_col(row, col):
     for i in range(length):
         value = board[i][col]
-        if value != "." and value not in permitted:
+        if value == "c":
             return False
     for j in range(length):
         value = board[row][j]
-        if value != "." and value not in permitted:
+        if value == "c":
             return False
     return True
 
 def _okay_surrounding(row, col):
     for i, j in surrounding:
-        if (row +i, col + j) in coor_list:
-            value = (row + i, col + j)
+        value = (row + i, col + j)
+        if value in coor_list:
             if value in setup_config:
                 max = setup_config[(row+i, col +j)]
                 if max == 0:
                     return False
-            elif board[value[0]][value[1]] != ".":
+            elif board[value[0]][value[1]] == "c":
                 return False
-    return True
-
-def full_affected_check():
-    for row in board:
-        check = Counter(row)
-        if check["c"] == 0 and check["."] == 0 :
-            return False
-    for col in zip(*board[::1]):
-        check = Counter(col)
-        if check["c"] == 0 and check["."] == 0:
-            return False
     return True
 
 def is_okay(row, col):
     if (row, col) not in coor_list or (row, col) in traverse:
         return False
-    elif _okay_row_col(row, col) and _okay_surrounding(row, col) and full_affected_check():
+    elif _okay_row_col(row, col) and _okay_surrounding(row, col):
         return True
     return False
 
@@ -73,10 +70,16 @@ def number(cell, reverse = False):
         row = cell[0] + i
         col = cell[1] + j
         if (row, col) in setup_config:
-            if not reverse:
-                setup_config[(row, col)] -= 1
-            else:
-                setup_config[(row, col)] += 1
+            setup_config[(row, col)] += 1 if reverse else -1
+        elif (row, col) in coor_list:
+            board[row][col] = "." if reverse else "-"
+    row, col = cell
+    for i in range(length):
+        if board[i][col] not in permitted:
+            board[i][col] = "." if reverse else "-"
+    for j in range(length):
+        if board[row][j] not in permitted:
+            board[row][j] = "." if reverse else "-"
 
 def number_check():
     """A look up to see if all the numbered square have been fullfill"""
@@ -109,20 +112,19 @@ def backtrack():
             row = location[0] + i
             col = location[1] + j
             if is_okay(row, col):
-                coor_list.remove((row, col))
                 recursion(row, col)
     else:
         for i, j in coor_list:
             if is_okay(i, j):
                 recursion(i, j)
                      
-
 layout_search()
 backtrack()
-if solved:
-    print(traverse)
+if not solved:
+    print(f"Steps: {count}, Unsolvable")
 else:
-    print("Unsolvable")
-print(count)
+    print(f"Steps: {count}", traverse)
+    for row in board:
+        print(row)
                 
     
