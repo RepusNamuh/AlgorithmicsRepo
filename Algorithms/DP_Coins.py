5b63b1a0cb75ebc7fb764bde2682772d488dde57
Repus <congle310610@gmail.com>
2024-07-16 20:00:01 +1000

    Update the file because it doesn't work previously

--- Algorithms/DP_Coins.py
+++ Algorithms/DP_Coins.py
@@ -1,17 +1,18 @@
 from collections import defaultdict
 
-def DPminOP(n, operations, A):
+def DPminOP(n, operations, values, A):
   A[1] = 1
   for i in range(2, n + 1):
     A[i] = i
     j = 0
-    for key, value in operations:
-        if key == "/" and ((i % value) == 0):
+    for opt in  operations:
+        value = values[j]
+        if opt == "/" and ((i % value) == 0):
             if i / value == 1:
                 A[i] = 1
             if A[i / value] + 1 < A[i]:
                 A[i] = A[i / value] + 1
-        elif key == "-" and (i - value) >= 1:
+        elif opt == "-" and (i - value) >= 1:
             if i - value == 1:
                 A[i] = 1
             elif A[i - value] + 1 < A[i]:
@@ -20,8 +21,9 @@ def DPminOP(n, operations, A):
       
   return A[n]
 n = 15
-operations = {"-": 1, "/": 2, "/": 3, "-":5}
+operations = ["-", "/", "/", "-"]
+values = [1, 2, 3, 5]
 A = defaultdict(int)
 
-print(DPminOP(n, operations, A))
+print(DPminOP(n, operations, values, A))
         
