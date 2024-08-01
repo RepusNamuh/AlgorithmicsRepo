46f50d58aaa0895e68733a2f1cc6f5c817bd51f6
Repus <congle310610@gmail.com>
2024-08-01 14:46:42 +1000

    Adding Simulated Annealing for Sum of Subset problems to Algorithms

---
+++ Algorithms/Simulated_Annealing_sample.py
@@ -0,0 +1,52 @@
+import random, math
+
+def subset(X, A):
+    Y = X.copy()
+    i = random.randint(0, len(A) - 1)
+    j = random.randint(0, len(A) - 1)
+    if A[i] in X:
+        Y.remove(A[i])
+    else:
+        Y.append(A[i])
+    if A[j] in X and A[j] in Y:
+        Y.remove(A[j])
+    else:
+        Y.append(A[j])
+    
+    return Y
+
+def Simulated_Annealing(A, d, error, step_size):
+    X = []
+    rand_int = random.randint(0, len(A) - 1)
+    for i in range(rand_int):
+        rand_index = random.randint(0, len(A) - 1)
+        X.append(A[rand_index])
+
+    lower_threshold = d - error
+    upper_threshold = d + error
+    T = 1.0
+    T_min = 0.00001
+    smallest_diff = math.inf
+    
+    while T > T_min and (lower_threshold > sum(X) or sum(X) > upper_threshold):
+        for i in range(step_size):
+            X_new = subset(X, A)
+            diff1 = abs(sum(X_new) - d)
+            diff2 = abs(sum(X) - d)
+            abs_diff = abs(diff1 - diff2)
+            rand_prob = random.random()
+            prob = math.exp(-abs_diff / T)
+
+            if diff1 < diff2:
+                X = X_new
+            elif diff1 > diff2 and prob > rand_prob:
+                X = X_new
+        smallest_diff = min(smallest_diff, abs_diff)
+        T = T * 0.99
+
+    return X, smallest_diff
+
+A = [2, 3, 5, 6, 8, 10]
+d = 10
+
+print(Simulated_Annealing(A, d, 0.5, 100))
